152cb0e2b928616802253a64e6abcc804743b058
Rodrigo Costa <rodrigo.costa@gmail.com>
2023-10-21 15:53:23 -0300

    add: string validator

--- StringValidator/string_validator.py
+++ StringValidator/string_validator.py
@@ -1,23 +1,21 @@
 if __name__ == '__main__':
     s = input()
-    if __name__ == '__main__':
-        s = input()
-        print(any(x.isalnum() for x in s))
-        print(any(x.isalpha() for x in s))
-        print(any(x.isdigit() for x in s))
-        print(any(x.islower() for x in s))
-        print(any(x.isupper() for x in s))
-    # hasAlfNum, hasAlfa, hasDigit, hasLower, hasUpper = False, False, False, False, False
-    # for c in s:
-    #     if c.isalnum():
-    #         hasAlfNum = True
-    #         if c.isalpha():
-    #             hasAlfa = True
-    #             if c.islower():
-    #                 hasLower = True
-    #             elif c.isupper():
-    #                 hasUpper = True
-    #         elif c.isdigit():
-    #             hasDigit = True
-    #
-    # print(f'{hasAlfNum}\n{hasAlfa}\n{hasDigit}\n{hasLower}\n{hasUpper}')
+    print(any(x.isalnum() for x in s))
+    print(any(x.isalpha() for x in s))
+    print(any(x.isdigit() for x in s))
+    print(any(x.islower() for x in s))
+    print(any(x.isupper() for x in s))
+# hasAlfNum, hasAlfa, hasDigit, hasLower, hasUpper = False, False, False, False, False
+# for c in s:
+#     if c.isalnum():
+#         hasAlfNum = True
+#         if c.isalpha():
+#             hasAlfa = True
+#             if c.islower():
+#                 hasLower = True
+#             elif c.isupper():
+#                 hasUpper = True
+#         elif c.isdigit():
+#             hasDigit = True
+#
+# print(f'{hasAlfNum}\n{hasAlfa}\n{hasDigit}\n{hasLower}\n{hasUpper}')
